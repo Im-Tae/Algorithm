113f46a418a0af4863817a15973e794a8cb1bf60
Im-Tae <dlaxo2420@naver.com>
2020-02-07 21:06:41 +0900

    1000, 1001

---
+++ src/_01/_1000.kt
@@ -0,0 +1,15 @@
+package _01
+
+/*
+
+import java.util.*
+
+val scanner = Scanner(System.`in`)
+
+fun main() = println(plus(getNum(), getNum()))
+
+fun getNum(): Int = scanner.nextInt()
+
+fun plus(num1: Int, num2: Int): Int = num1 + num2
+
+ */
---
+++ src/_01/_1001.kt
@@ -0,0 +1,15 @@
+package _01
+
+/*
+
+import java.util.*
+
+val scanner = Scanner(System.`in`)
+
+fun main() = println(minus(getNum(), getNum()))
+
+fun getNum(): Int = scanner.nextInt()
+
+fun minus(num1: Int, num2: Int): Int = num1 - num2
+
+ */
